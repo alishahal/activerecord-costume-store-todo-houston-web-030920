6cd507c4b3cda4243bd0768179522f21ec276679
Karim Macknojia <alishahal@msn.com>
2020-03-24 01:47:35 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,13 +10,14 @@
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
   def change
-    create_table(:costumes) |table|
+    create_table(:costumes) do |table|
      table.string(:name)
      table.float(:price)
      table.string(:size)
      table.string(:image_url)
      
      table.timestamps
+    end
   end
   
 end
