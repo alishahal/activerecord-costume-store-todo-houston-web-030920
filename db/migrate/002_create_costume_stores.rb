3b62378ee9381e04291f40834bde3af0a0bad2ac
Karim Macknojia <alishahal@msn.com>
2020-03-24 01:59:22 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       table.string(:name)
       table.string(:loation)
       table.integer(:costume_inventory)
-      table.integer(:num_employees)
-      table.boolean(:in_business?)
+      table.integer(:num_of_employees)
+      table.boolean(:still_in_business?)
       table.datetime(:opening_time)
       table.datetime(:closing_time)
    end
