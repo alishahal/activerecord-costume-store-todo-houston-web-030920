84edf3be441888358b767e5d6d63b306eaf99749
Karim Macknojia <alishahal@msn.com>
2020-03-24 01:48:21 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 class HauntedHouses < ActiveRecord::Migration[5.2]
-  create_table(:haunted_houses) |table|
+  create_table(:haunted_houses) do |table|
       table.string(:name)
       table.string(:loation)
       table.string(:theme)
@@ -9,6 +9,7 @@ class HauntedHouses < ActiveRecord::Migration[5.2]
       table.string(:opening_date)
       table.string(:closing_date)
       table.string(:long_description)
+     end
   end
   
 end
