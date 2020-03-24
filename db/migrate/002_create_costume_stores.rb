a54c47003b4b64a652c69dba9e26408fb18dcaed
Karim Macknojia <alishahal@msn.com>
2020-03-24 01:25:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+class CostumeStores < ActiveRecord::Migration[5.2]
+  create_table(:costume_stores) |table|
+      table.string(:name)
+      table.string(:loation)
+      table.string(:costume_inventory)
+      table.integer(:employees)
+      table.boolean(:in_business?)
+      table.string(:closing_time)
+  end
+  
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration[5.2]
+  create_table(:haunted_houses) |table|
+    table.string(:name)
+      table.string(:loation)
+      table.string(:theme)
+      table.integer(:price)
+      table.boolean(:family_friendly?)
+      table.string(:opening_date)
+      table.string(:closing_date)
+      table.string(:long_description)
+  end
+  
+end
